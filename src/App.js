f2919ccc66ce2e753f91a00d666ffc38aa40ea90
JerzyFrodsom <53261544+JerzyFrodsom@users.noreply.github.com>
2019-07-24 18:43:51 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ function App() {
     <div className="App">
       <section className="page">
         <header className="header">
-          <h1 className="header__text">Narodowy Sympatyk</h1>
+          <div className="header__logo"><img src={logo} alt="falanga" className="logo"/><h1 className="header__text">Narodowy Sympatyk</h1></div>
+          <ul className="nav" id="blog">
+          <li className="nav__item"><a className="nav__link" href="#blog">Blog</a></li>
+          <li className="nav__item"><a href="https://www.onr.com.pl" className="nav__link" target="_blank">O nas</a></li>
+          <li className="nav__item"><a href="#footer" className="nav__link">Kontakt</a></li>
+          </ul>
         </header>
         <div className="main">
           <div className="main__box--flex">
@@ -28,14 +33,14 @@ function App() {
             <img
               src={sympatyk}
               alt="narodowy_sympatyk_widok_mobilny"
-              class="main__img"
+              className="main__img"
             />
           </div>
         </div>
       </section>
       <section className="page">
         <div className="main">
-          <div className="main__photo"><img src={sympatyk} alt="narodowy_sympatyk_widok_mobilny" class="main__img"/></div>
+          <div className="main__photo"><img src={sympatyk} alt="narodowy_sympatyk_widok_mobilny" className="main__img"/></div>
           <div className="main__box">
             <h4 className="main__paragraph">Narodowy Sympatyk to platforma informacyjna zbudowana wokół szlachetnego świata patriotycznego zaangażowania.</h4>
             <p className="main__paragraph--small">Powstał, by udostępniać informacje o działalności narodowej, promować nowe inicjatywy i zachęcać do pogłębiania swojej wiedzy. Narodowy Sympatyk to krok do własnego zaangażowania, powzięcia sprawy w swoje ręce, to także ideowy przekaz dla wszystkich zainteresowanych radykalną zmianą swojego życia. Aplikacja jest mobilną sumą wydarzeń, artykułów, literatury i muzyki o tematyce patriotycznej. </p>
@@ -43,7 +48,7 @@ function App() {
           </div>
         </div>
       </section>
-
+      <footer id="footer" className="footer"><p className="footer__text">Kontakt: <a href="mailto:narodowy.sympatyk@onr.com.pl" className="footer__link">narodowy.sympatyk@onr.com.pl</a></p></footer>
     </div>
   );
 }
